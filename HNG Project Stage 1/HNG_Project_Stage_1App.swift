18ce3d88684f66af32395a138a996a2f0e921a7a
David Olagookun <erijesudo@gmail.com>
2023-09-08 20:05:09 +0100

    Initial Commit

---
+++ HNG Project Stage 1/HNG_Project_Stage_1App.swift
@@ -0,0 +1,17 @@
+//
+//  HNG_Project_Stage_1App.swift
+//  HNG Project Stage 1
+//
+//  Created by David OH on 08/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct HNG_Project_Stage_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
